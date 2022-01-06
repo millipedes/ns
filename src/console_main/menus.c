4a6a26eac75cb65ecfa571d1820b32ba0bf4127b
millipedes <millipedes@earthlink.net>
2022-01-06 00:27:45 -0700

    update commit

--- src/console_main/menus.c
+++ src/console_main/menus.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 
 void print_welcome_screen(void) {
-    printf("------------------------------\n");
-    printf("\t\tWelcome\n");
-    printf("------------------------------\n");
+    printf("------------------------------------------\n");
+	printf("\t███╗   ██╗███████╗\n");
+	printf("\t████╗  ██║██╔════╝\n");
+	printf("\t██╔██╗ ██║███████╗\n");
+	printf("\t██║╚██╗██║╚════██║\n");
+	printf("\t██║ ╚████║███████║\n");
+	printf("\t╚═╝  ╚═══╝╚══════╝\n");
+    printf("------------------------------------------\n");
+	printf("Welcome to the REPL for the ns programming\n");
+	printf("language.  For help please visit:\n");
+	printf("https://github.com/millipedes/ns\n");
+    printf("------------------------------------------\n");
 }
